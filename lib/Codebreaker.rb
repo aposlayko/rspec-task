1a05e2fa5aa66403f6da6aa3be2ce0dd9caed314
Alexandr Poslayko <aposlayko@gmail.com>
2015-05-20 20:28:50 +0300

    codebreaker init

---
+++ lib/Codebreaker.rb
@@ -0,0 +1,7 @@
+require 'codebreaker/version'
+require 'codebreaker/game'
+
+module Codebreaker
+  # Your code goes here...
+  puts 'hello'
+end
---
+++ spec/codebreaker/game_spec.rb
@@ -0,0 +1,11 @@
+require '../spec_helper'
+
+module Codebreaker
+  describe Game do
+    describe '#start' do
+      it 'generates secret code'
+      it 'saves 4 numbers secret code'
+      it 'saves secret code with numbers from 1 to 6'
+    end
+  end
+end
